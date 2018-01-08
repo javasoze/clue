cc21726389919f54fc3a5771e901efd07bba2d5a
John Wang <john@dashbase.io>
2018-01-08 00:08:29 -0800

    6.2.0 release

--- bin/build_sample_index.sh
+++ bin/build_sample_index.sh
@@ -11,4 +11,4 @@ JAVA_OPTS="-server -d64"
 
 MAIN_CLASS="com.senseidb.clue.test.BuildSampleIndex"
 
-java $JAVA_OPTS $JMX_OPTS $HEAP_OPTS -cp $dist/clue-6.2.0-0.0.1-SNAPSHOT.jar $MAIN_CLASS $bin/../src/main/resources/cars.json $@
+java $JAVA_OPTS $JMX_OPTS $HEAP_OPTS -cp $dist/clue-6.2.0-1.0.0.jar $MAIN_CLASS $bin/../src/main/resources/cars.json $@
--- bin/clue.sh
+++ bin/clue.sh
@@ -12,5 +12,5 @@ HEAP_OPTS="-Xmx1g -Xms1g -XX:NewSize=256m"
 JAVA_OPTS="-server -d64 -Dhadoop.conf.dir=$HADOOP_CONF_DIR"
 #JAVA_DEBUG="-Xdebug -Xrunjdwp:transport=dt_socket,address=1044,server=y,suspend=y"
 
-(cd $bin/..; java $JAVA_OPTS $JAVA_DEBUG $HEAP_OPTS -jar $dist/clue-6.2.0-0.0.1-SNAPSHOT.jar $@)
+(cd $bin/..; java $JAVA_OPTS $JAVA_DEBUG $HEAP_OPTS -jar $dist/clue-6.2.0-1.0.0.jar $@)
 
