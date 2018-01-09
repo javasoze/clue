acbe4c7738228b93b8d5a7f6c402e0c378da774b
John Wang <john@dashbase.io>
2018-01-08 17:28:16 -0800

    fix script

--- bin/build_sample_index.sh
+++ bin/build_sample_index.sh
@@ -9,6 +9,6 @@ dist=$bin/../target
 HEAP_OPTS="-Xmx1g -Xms1g -XX:NewSize=256m"
 JAVA_OPTS="-server -d64"
 
-MAIN_CLASS="BuildSampleIndex"
+MAIN_CLASS="io.dashbase.clue.test.BuildSampleIndex"
 
 java $JAVA_OPTS $JMX_OPTS $HEAP_OPTS -cp $dist/clue-6.6.2-1.0.0.jar $MAIN_CLASS $bin/../src/main/resources/cars.json $@
