edae98389e11c1e75d2b94df0df6c34dc7299d82
John Wang <john.wang@gmail.com>
2019-03-24 12:14:36 -0700

    remove hadoop dependency (#65)

--- src/main/java/io/dashbase/clue/api/DefaultDirectoryBuilder.java
+++ src/main/java/io/dashbase/clue/api/DefaultDirectoryBuilder.java
@@ -1,5 +1,8 @@
 package io.dashbase.clue.api;
 
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
 import java.io.IOException;
 import java.net.URI;
 import java.nio.file.FileSystems;
@@ -7,17 +10,10 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-import io.dashbase.clue.HdfsDirectory;
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.Path;
-import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.FSDirectory;
-import org.apache.lucene.store.NoLockFactory;
-
 public class DefaultDirectoryBuilder implements DirectoryBuilder {
 
-  private static final Set<String> SUPPORTED_SCHEMES = new HashSet<String>(Arrays.asList("file", "hdfs"));
-  private static final String HADOOP_CONFIFG_DIR = "hadoop.conf.dir";
+  private static final Set<String> SUPPORTED_SCHEMES = new HashSet<String>(Arrays.asList("file"));
+
   @Override
   public Directory build(URI idxUri) throws IOException {
     String scheme = idxUri.getScheme();
@@ -28,28 +24,6 @@ public class DefaultDirectoryBuilder implements DirectoryBuilder {
     if (SUPPORTED_SCHEMES.contains(scheme)) {
       if ("file".equals(scheme)) {
         return FSDirectory.open(FileSystems.getDefault().getPath(idxUri.getPath()));
-      }
-      else if ("hdfs".equals(scheme)){
-        String hadoopConfDir = null;
-        String filePath = idxUri.getPath();
-        int delimIdx = filePath.indexOf("@");
-        if (delimIdx >= 0) {
-          hadoopConfDir = filePath.substring(delimIdx + 1);
-          filePath = filePath.substring(0, delimIdx);
-        }
-        if (hadoopConfDir == null) {
-          hadoopConfDir = System.getProperty(HADOOP_CONFIFG_DIR);
-        }
-        Configuration config = new Configuration();
-        if (hadoopConfDir != null && hadoopConfDir.trim().length() > 0) {
-          System.out.println("Hadoop configuration found at: " + hadoopConfDir);
-          Path hadoopConfPath = new Path(hadoopConfDir);
-          config.addResource(new Path(hadoopConfPath,"core-site.xml"));
-          config.addResource(new Path(hadoopConfPath,"hdfs-site.xml"));
-          config.addResource(new Path(hadoopConfPath,"mapred-site.xml"));
-        }
-        Path hdfsPath = new Path(filePath);        
-        return new HdfsDirectory(NoLockFactory.INSTANCE, hdfsPath, config);
       }
       else {
         throw new IOException("unsupported protocol: " + scheme);
