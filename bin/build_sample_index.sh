6a55efec04ede9ebc93c54f892bdbc7bd3fa7d27
John Wang <john.wang@gmail.com>
2019-09-01 21:33:22 -0700

    Lucene 8 (#71)
    * upgrade lucene 8.0
    
    * clean up

--- bin/build_sample_index.sh
+++ bin/build_sample_index.sh
@@ -3,7 +3,7 @@
 bin=`dirname "$0"`
 bin=`cd "$bin"; pwd`
 
-lib=$bin/../target/lib
+lib=$bin/../target/libs
 dist=$bin/../target
 
 HEAP_OPTS="-Xmx1g -Xms1g -XX:NewSize=256m"
@@ -11,4 +11,4 @@ JAVA_OPTS=""
 
 MAIN_CLASS="io.dashbase.clue.test.BuildSampleIndex"
 
-java $JAVA_OPTS $JMX_OPTS $HEAP_OPTS -cp $dist/clue-*.jar $MAIN_CLASS $bin/../src/main/resources/cars.json $@
+java $JAVA_OPTS $JMX_OPTS $HEAP_OPTS -cp $dist/*:$lib/* $MAIN_CLASS $bin/../src/main/resources/cars.json $@
