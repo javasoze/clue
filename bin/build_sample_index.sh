9ed88ade2aa4a2be180ab7c7fb08594b5a0717c6
John Wang <john.wang@gmail.com>
2025-12-19 15:13:46 -0500

    moved to gradle from maven (#85)
    Co-authored-by: John Wang <cocobeach1911@gmail.com>

--- bin/build_sample_index.sh
+++ bin/build_sample_index.sh
@@ -3,12 +3,11 @@
 bin=`dirname "$0"`
 bin=`cd "$bin"; pwd`
 
-lib=$bin/../target/libs
-dist=$bin/../target
+lib=$bin/../build/libs
 
 HEAP_OPTS="-Xmx1g -Xms1g -XX:NewSize=256m"
 JAVA_OPTS=""
 
 MAIN_CLASS="io.dashbase.clue.test.BuildSampleIndex"
 
-java $JAVA_OPTS $JMX_OPTS $HEAP_OPTS -cp $dist/*:$lib/* $MAIN_CLASS $bin/../src/main/resources/cars.json $@
+java $JAVA_OPTS $JMX_OPTS $HEAP_OPTS -cp $lib/* $MAIN_CLASS $bin/../src/main/resources/cars.json $@
