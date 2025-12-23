cc7919a9d6d718574f3b7423198a49e1fc8c14b2
John Wang <john.wang@gmail.com>
2025-12-23 11:29:58 -0500

    clean up config and enhanced info cmd (#93)
    Co-authored-by: John Wang <cocobeach1911@gmail.com>

--- src/main/java/io/dashbase/clue/api/DirectoryProvider.java
+++ src/main/java/io/dashbase/clue/api/DirectoryProvider.java
@@ -1,12 +1,11 @@
 package io.dashbase.clue.api;
 
-import org.apache.lucene.store.Directory;
-
 import java.io.IOException;
-import java.util.Map;
+
+import org.apache.lucene.store.Directory;
 
 public interface DirectoryProvider {
   String getName();
 
-  Directory build(String location, Map<String, String> options) throws IOException;
+  Directory build(String location, ParsedOptions options) throws IOException;
 }
